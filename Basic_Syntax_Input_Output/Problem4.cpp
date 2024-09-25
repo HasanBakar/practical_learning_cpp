03ce6c93fc4c5ff1233ae5131fd795b7e756c408
HasanBakar <abu.bakar.cse.uctc@gmail.com>
2024-09-26 00:42:43 +0600

    :new: :tada: :star: Problem 4 solved using swapping

---
+++ Basic_Syntax_Input_Output/Problem4.cpp
@@ -0,0 +1,16 @@
+/*
+✅Problem 4: Write a program to swap two numbers using a third variable.
+
+ */
+#include <iostream>
+using namespace std;
+int main(){
+    int a, b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    cout << "After swapping: " << a << " " << b;
+    return 0;
+}
