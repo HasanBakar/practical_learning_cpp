16fd9defed15c4f0c95ae5d9dadc1503e7e79429
HasanBakar <abu.bakar.cse.uctc@gmail.com>
2024-09-25 23:47:57 +0600

    :new: :tada: :star: Problem 1 solved

---
+++ Basic_Syntax_Input_Output/Problem1.cpp
@@ -0,0 +1,13 @@
+/*
+✅ Problem 1: Write a program to take input of two integers and output their sum.
+*/
+
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+    cout << "Sum = " << a + b;
+    return 0;
+}
