54c6111480f880df29ea38cfe467bfd6bbd36728
HasanBakar <abu.bakar.cse.uctc@gmail.com>
2024-09-30 08:11:14 +0600

    :new: :tada: :star: Conditional Statements problem 1

---
+++ Conditional_Statements/Problem1.cpp
@@ -0,0 +1,17 @@
+/*
+✅ Problem 1: Write a program to check if a given number is even or odd.
+ */
+
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    if (n % 2 == 0) {
+        cout << "The number is even." << endl;
+    } else {
+        cout << "The number is odd." << endl;
+    }
+    return 0;
+}
